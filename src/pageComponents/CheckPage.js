059876f18fc58d433c49114cf38b9d0aa231d6c0
George Deeks <georgedeeks@gmail.com>
2019-11-15 22:05:08 +0000

    fix: form part one

--- src/pageComponents/CheckPage.js
+++ src/pageComponents/CheckPage.js
@@ -1,39 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field/*, ErrorMessage */ } from 'formik';
+import isEmpty from 'lodash/isEmpty';
 
 import VerticalSpacing from '../components/VerticalSpacing';
 import Paragraph from "antd/lib/typography/Paragraph";
-import { OmitProps } from "antd/lib/transfer/renderListBody";
+
+import FormikRadioGroup from './checkPage/FormikRadioGroup';
+
+// import { OmitProps } from "antd/lib/transfer/renderListBody";
 
 const Label = styled.label`
 	margin-bottom: 1rem; 
 `;
-
-const CheckboxLabel = styled(Label)`
-	display: block;
-	position: relative;
-	padding-left: 5%;
-	margin-bottom: 1rem;
-	cursor: pointer;
-	-webkit-user-select: none;
-	-moz-user-select: none;
-	-ms-user-select: none;
-	user-select: none;
-
-	> input {
-		position: absolute;
-    opacity: 0;
-    cursor: pointer;
-    height: 0;
-		width: 0; 	
-	}
-`;
-
-const LabelParagraph = styled.p`
-	font-size: 20px;
-	font-weight: 500;
-`
 
 const Input = styled.input`
 	:hover, :focus {
@@ -41,26 +20,26 @@ const Input = styled.input`
 	}
 `;
 
-const BlockInput = styled(Input)`
-	display: block;
-	width: 100%;
-	border: 2px solid #606060;
-	padding: 5px;
-`;
+// const BlockInput = styled(Input)`
+// 	display: block;
+// 	width: 100%;
+// 	border: 2px solid #606060;
+// 	padding: 5px;
+// `;
 
-const Textarea = styled.textarea`
-	display: block;
-	width: 100%;
-	border: 2px solid #606060;
-	padding: 5px;
+// const Textarea = styled.textarea`
+// 	display: block;
+// 	width: 100%;
+// 	border: 2px solid #606060;
+// 	padding: 5px;
 
-	:hover, :focus {
-		outline: 1px yellow solid;
-	}
-`;
+// 	:hover, :focus {
+// 		outline: 1px yellow solid;
+// 	}
+// `;
 
 const Heading = styled.h2`
-
+	margin: 2rem 0 0 0;
 `;
 
 const List = styled.ul`
@@ -73,16 +52,27 @@ const List = styled.ul`
 `;
 
 const Button = styled.button`
-  border: 2px solid #2196F3;
   background: none;
   padding: 5px 15px;
   -webkit-transition: 0.3s ease;
   transition: 0.3s ease;
+	
+	${props => !props.disabled && `
+		border: 2px solid #2196F3;
+		
+		:hover {
+			background: #2196F3;
+			color: white; 
+		}
+	`}	
 
-	:hover {
-    background: #2196F3;
-		color: white; 
-	}
+	${props => props.showAllForm && `
+		background: green;
+		color: white;
+		font-weight: 700;
+	`}	
+
+
 `;
 
 const Wrapper = styled.div`	
@@ -103,13 +93,40 @@ const FieldParagraph = styled(Paragraph)`
 	margin-right: 1rem;
 `;
 
-const ListWrapper = styled.div``;
+const QuestionWrapper = styled.div``;
 
 const RadioButtonWrapper = styled.div``;
 
+const FlexRow = styled.div`
+	display: flex;
+	flex-direction: row;
+`;
+
+const FlexColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const StatusBox = styled.div`
+	background-color: 'white';
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	margin: 3rem 1rem;
+`;
+
+const ErrorText = styled.div`
+	color: red;
+	margin-bottom: 1rem;
+`;
+
+const RequiredParagraph = styled(Paragraph)`
+	margin-bottom: 0.75rem;
+`;
+
 const CheckPage = () => {
 
-	const [radioValue, setRadioValue] = React.useState(null);
+	const [showAllForm, setShowAllForm] = React.useState(false);
 
 
 	return (
@@ -119,13 +136,21 @@ const CheckPage = () => {
 				initialValues={{ 
 					//email: '',
 					listConditions: '',
+					universalCredit: '',
+					childTaxCredit: '',
 				}}
 				validate={values => {
 					const errors = {};
 					
-					if (!values.listConditions) {
-						errors.listConditions = 'Required';
+					// TODO remove error if sufficient condition has been met
+					if (!values.universalCredit) {
+						errors.universalCredit = 'Please answer all questions';
 					}
+
+					if (!values.childTaxCredit) {
+						errors.childTaxCredit = 'Please answer all questions';
+					}
+
 					
 					// if (!values.email) {
 					// 	errors.email = 'Required';
@@ -153,85 +178,124 @@ const CheckPage = () => {
 				}) => {	
 					const setListConditionsField = value => {
 						setFieldValue('listConditions', value);
+						setFieldValue('universalCredit', values.universalCredit)
+						setFieldValue('childTaxCredit', values.childTaxCredit)
 					};
-					
+					const setUniversalCreditField = value => {
+						setFieldValue('listConditions', values.listConditions);
+						setFieldValue('universalCredit', value)
+						setFieldValue('childTaxCredit', values.childTaxCredit)
+					};
+					const setChildTaxCreditField = value => {
+						setFieldValue('listConditions', values.listConditions);
+						setFieldValue('universalCredit', values.universalCredit)
+						setFieldValue('childTaxCredit', value)
+					};
+
+					const getInitialFormStatusString = () => {
+						if (
+							values.universalCredit === ''
+							||
+							values.childTaxCredit === ''
+						) {
+							return 'Please complete all required fields before clicking the \'check\' button.';
+						}
+						
+						return 'Click to check your status!';
+					} 
+
 					return (
 						<Form>
 
 							<Wrapper>
 
-								<ListWrapper>
+								<QuestionWrapper>
 								
 									<Heading>Do you receive any of the following?</Heading>
 
-									<List>
-										<li>Income Support</li>
-										<li>Income-based Job Seeker Allowance (JSA)</li>
-										<li>Income-related Employment and Support Allowance (ESA)</li>
-										<li>Support under Part 6 of the Immigration and Asylum Act 1999</li>
-										<li>The guarantee element of Pension Credit</li>
-										<li>Working Tax Credit run-on (paid for the four weeks after you stop qualifying for Working Tax Credit)</li>
-										<li>Universal Credit where the yearly combined awarded amount for your household is less than £7,400</li>
-										<li>Child Tax Credit (with no Working Tax Credit) with an annual income of no more than £16,190</li>
-									</List>
+									<FlexRow>
+										<List>
+											<li>Income Support</li>
+											<li>Income-based Job Seeker Allowance (JSA)</li>
+											<li>Income-related Employment and Support Allowance (ESA)</li>
+											<li>Support under Part 6 of the Immigration and Asylum Act 1999</li>
+											<li>The guarantee element of Pension Credit</li>
+											<li>Working Tax Credit run-on (paid for the four weeks after you stop qualifying for Working Tax Credit)</li>
+										</List>
 
-									<Field
-										name="listConditions"
-									>
-										{({ field }) => (
-											<>
-												<RadioButtonWrapper>
-													<input
-														{...field}
-														id="yes"
-														value="yes"
-														checked={field.value === 'yes'}
-														name="type"
-														type="radio"
-														onChange={() => setListConditionsField('yes')}
-													/>
-													<label htmlFor="yes">Yes</label>
-												</RadioButtonWrapper>
+										<Field
+											name="listConditions"
+											options={["Yes", "No", "Maybe"]}
+											component={FormikRadioGroup}
+										/>
+									</FlexRow>									
+							</QuestionWrapper>						
 
-												<RadioButtonWrapper>
-													<input
-														{...field}
-														id="maybe"
-														value="maybe"
-														name="type"
-														checked={field.value === 'maybe'}
-														type="radio"
-														onChange={() => setListConditionsField('maybe')}
-													/>
-													<label htmlFor="maybe">Maybe</label>
-												</RadioButtonWrapper>
+							<QuestionWrapper>
+							
+								<Heading>Universal Credit*</Heading>
+								<RequiredParagraph>(*This field is required.)</RequiredParagraph>
+								<Paragraph>Do you receive Universal Credit where the yearly combined awarded amount for your household is less than £7,400?</Paragraph>
 
-												<RadioButtonWrapper>
-													<input
-														{...field}
-														id="no"
-														value="no"
-														name="type"
-														checked={field.value === 'no'}
-														type="radio"
-														onChange={() => setListConditionsField('no')}
-													/>
-													<label htmlFor="no">No</label>
-												</RadioButtonWrapper>
-											</>
-										)}
-									</Field>
+								<Field
+									name="universalCredit"
+									options={["Yes", "No", "Maybe"]}
+									component={FormikRadioGroup}
+								/>
 
-									{errors.listConditions && <div>EVERYTHING IS WRONG</div>}
-							</ListWrapper>									
-							{/*
-							<Field type="email" name="email" />
-							<ErrorMessage name="email" component="div" />
-							*/}
-							<button type="submit" disabled={isSubmitting}>
-								Submit
-							</button>
+
+							</QuestionWrapper>
+
+							<QuestionWrapper>
+							
+								<Heading>Child Tax Credit*</Heading>
+								<RequiredParagraph>(*This field is required.)</RequiredParagraph>
+								<Paragraph>
+									Do you receive Child Tax Credit (with no Working Tax Credit) with an annual income of less than £16,190?	
+								</Paragraph>
+
+								<Field
+									name="childTaxCredit"
+									options={["Yes", "No", "Maybe"]}
+									component={FormikRadioGroup}
+								/>
+
+							</QuestionWrapper>		
+							
+
+							<StatusBox>
+	
+								<div>{getInitialFormStatusString()}</div>
+								
+								<Button 
+									onClick={() => setShowAllForm(true)} 
+									disabled={
+										!isEmpty(errors) || !values.universalCredit || !values.childTaxCredit || showAllForm
+									}
+									showAllForm={showAllForm}
+								>
+									{showAllForm ? 'You may be eligible! Continue below' : 'Check'}
+								</Button>
+							</StatusBox>
 						</Wrapper>
+
+						{showAllForm && (
+							<>
+							
+								<div>IGNORE BELOW</div>
+
+								<Button 
+									type="submit" 
+									disabled={
+										!isEmpty(errors) || isSubmitting
+									}
+								>
+									Check
+								</Button>
+
+							</>
+						)}
+
 					</Form>
 				)}}
 			</Formik>			
